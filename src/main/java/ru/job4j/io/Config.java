4f4b6255a0ff759dd92d4a01e087af719764fb65
Andrey Solarsky <SerSam-2007@yandex.ru>
2022-03-29 16:04:46 +0400

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,84 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class Config {
+
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+    }
+
+    /**
+     * Шаблон комментария: обязательно одиночный # и далее любые символы или конец строки
+     *
+     * Шаблон верного функционального присвоения состоит из частей:
+     * с начала строки любые символы, кроме #
+     * (чтобы не читались комментарии подходящие под функциональные строки, но пробелом перед #)
+     * обязательная группа больше одного буквенного (это и есть параметр)
+     * затем любые символы (пробелы, или часть параметра)
+     * обязательное =
+     * любые символы, больше одного буквенного, затем любые до конца строки
+     *
+     * Все остальные варианты - выброс исключения.
+     */
+    public boolean isLineInformative(String line) {
+        boolean rsl = false;
+        Pattern correctPattern = Pattern.compile("^(.[^#]+)?\\S+(.+)?=(.+)?\\S+(.+)?$");
+        Pattern commentPattern = Pattern.compile("^#(.+)?$");
+        if (line.length() > 0
+                && !line.matches(String.valueOf(commentPattern))) {
+            if (line.matches(String.valueOf(correctPattern))) {
+                rsl = true;
+            } else {
+                throw new IllegalArgumentException("The line is incorrect.");
+            }
+        }
+        return rsl;
+    }
+
+    public void load() {
+        List<String> array = new ArrayList<>();
+        String[] lineArr;
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(array::add);
+            for (String line : array) {
+                if (isLineInformative(line)) {
+                    lineArr = line.split("=");
+                    values.put(lineArr[0], lineArr[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        if (!values.containsKey(key)) {
+            throw new UnsupportedOperationException("Don't impl this method yet!");
+        }
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,80 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class ConfigTest {
+
+    /**
+     * name=Petr Arsentev
+     */
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"),is("Petr Arsentev"));
+    }
+
+    /**
+     * #привет мир!
+     *
+     * name=Andrey S, = no, not =
+     *
+     * #
+     *
+     * age=33
+     */
+    @Test
+    public void whenPairWithCommentAndEmptyLine() {
+        String path = "./data/pair_with_comments_and_empty_lines.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"),is("Andrey S, "));
+        assertThat(config.value("age"),is("33"));
+    }
+
+    /**
+     * name=
+     * Ivan
+     * =Ivan
+     * name=Andrey S, = no, not =
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void whenPairWithWrongPattern() {
+        String path = "./data/pair_with_incorrect_pattern.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    /**
+     * name=
+     * Ivan
+     * =Ivan
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void whenOnlyWrongPattern() {
+        String path = "./data/only_incorrect_pattern.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"),is("Andrey"));
+    }
+
+    @Test
+    public void isLineCorrectTest() {
+        Config conf = new Config("./");
+        assertTrue(conf.isLineInformative(" Wonderful = world "));
+        assertTrue(conf.isLineInformative(" Wond erful = wor ld "));
+        assertTrue(conf.isLineInformative(" 12n!@#de$rful = wor!@#$%^&*(l "));
+        assertTrue(conf.isLineInformative(" 1#2n!@=#de$rful = wor!@#$=%^&*(l "));
+
+        assertFalse(conf.isLineInformative("#dfWorld is wonderful"));
+        assertFalse(conf.isLineInformative("#Wonderful=world"));
+        assertFalse(conf.isLineInformative("# Wonde rful = wor ld "));
+    }
+}
