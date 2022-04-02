e3af328b5e9a95ddc292b92e0488b41e26a03d2c
Andrey Solarsky <SerSam-2007@yandex.ru>
2022-04-02 19:00:09 +0400

    2. Анализ доступности сервера. [#859] в мэйне теперь демонстрируется работа класса

--- src/main/java/ru/job4j/io/Analyze.java
+++ src/main/java/ru/job4j/io/Analyze.java
@@ -6,12 +6,7 @@ import java.util.List;
 
 public class Analyze {
     public static void main(String[] args) {
-        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
-            out.println("15:01:30;15:02:32");
-            out.println("15:10:30;23:12:32");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        new Analyze().unavailable("./server.log", "./unavailable.csv");
     }
 
     private boolean isLineAboutShutdown(String line) {
