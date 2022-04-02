62fa76825ac3d49f03cf49b75783c36b5c84335f
Andrey Solarsky <SerSam-2007@yandex.ru>
2022-04-02 18:27:29 +0400

    2. Анализ доступности сервера. [#859] вынес запись файла в отдельный метод

--- src/main/java/ru/job4j/io/Analyze.java
+++ src/main/java/ru/job4j/io/Analyze.java
@@ -6,14 +6,19 @@ import java.util.List;
 
 public class Analyze {
     public static void main(String[] args) {
-        unavailable("./server.log", "./unavailable.csv");
+        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
+            out.println("15:01:30;15:02:32");
+            out.println("15:10:30;23:12:32");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    private static boolean isLineAboutShutdown(String line) {
+    private boolean isLineAboutShutdown(String line) {
         return '4' == line.charAt(0) || '5' == line.charAt(0);
     }
 
-    public static void unavailable(String source, String target) {
+    public void unavailable(String source, String target) {
         List<String> resultList = new ArrayList<>();
         String[] lineAsArray;
         boolean isOn = true;
@@ -35,11 +40,15 @@ public class Analyze {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        writeToFile(resultList, target);
+    }
+
+    private void writeToFile(List<String> list, String target) {
         try (PrintWriter out = new PrintWriter(
                 new BufferedOutputStream(
                         new FileOutputStream(target)
                 ))) {
-            for (String line : resultList) {
+            for (String line : list) {
                 out.println(line);
             }
         } catch (IOException e) {
