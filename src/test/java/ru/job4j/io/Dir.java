608f8f0bcb21c79f30c6ccf4d4282c463dd08166
Andrey Solarsky <SerSam-2007@yandex.ru>
2022-04-05 17:08:30 +0400

    4.0. File [#252491]

--- src/test/java/ru/job4j/io/Dir.java
+++ src/test/java/ru/job4j/io/Dir.java
@@ -6,10 +6,12 @@ public class Dir {
     public static void main(String[] args) {
         File file = new File("c:\\projects");
         if (!file.exists()) {
-            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+            throw new IllegalArgumentException(
+                    String.format("Not exist %s", file.getAbsoluteFile()));
         }
         if (!file.isDirectory()) {
-            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+            throw new IllegalArgumentException(
+                    String.format("Not directory %s", file.getAbsoluteFile()));
         }
         for (File subfile : file.listFiles()) {
             if (!file.isDirectory()) {
