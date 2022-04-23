672eb623fd2b569ea923a6c1cd9e4322e401a229
Andrey Solarsky <SerSam-2007@yandex.ru>
2022-04-23 21:29:48 +0400

    5.2. Архивировать проект [#861]

---
+++ src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,57 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+
+    public static void main(String[] args) {
+        Zip zip = new Zip();
+        ArgsName argsNames = ArgsName.of(args);
+        if (!new File(argsNames.get("d")).exists()) {
+            throw new IllegalArgumentException("Directory not found.");
+        }
+        File zipFile = new File(argsNames.get("o"));
+        List<Path> fileList = new ArrayList<>();
+        try {
+            fileList = Search.search(Path.of(argsNames.get("d")),
+                    p -> !p.toFile().getName().endsWith(argsNames.get("e")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        zip.packFiles(fileList, zipFile);
+    }
+
+    public void packFiles(List<Path> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(
+                new BufferedOutputStream(
+                        new FileOutputStream(target)))) {
+            for (var file : sources) {
+                zip.putNextEntry(new ZipEntry(String.valueOf(file)));
+                try (BufferedInputStream out = new BufferedInputStream(
+                        new FileInputStream(String.valueOf(file)))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(
+                new BufferedOutputStream(
+                        new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
