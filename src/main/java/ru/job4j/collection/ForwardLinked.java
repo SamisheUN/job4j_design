62d5f9852dc9d8432906a760d1c1adc70e6a9bae
Andrey Solarsky <SerSam-2007@yandex.ru>
2023-06-24 17:21:46 +0400

    3. Удалить head в односвязном списке. [#51424]

---
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -0,0 +1,93 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class ForwardLinked<T> implements Iterable<T> {
+    private int size = 0;
+    private int modCount = 0;
+    private Node<T> head;
+
+    public void add(T value) {
+        ForwardLinked.Node<T> newNode = new ForwardLinked.Node(value, null);
+        if (null == head) {
+            head = newNode;
+        } else {
+            ForwardLinked.Node<T> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    public T get(int index) {
+        Objects.checkIndex(index, size);
+        ForwardLinked.Node<T> currentNode = head;
+        for (int i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+        return currentNode.item;
+    }
+
+    /**
+     * node trash = head
+     *
+     * head = head.next
+     * trash.next = null;
+     * trsh.
+     *
+     * @return
+     */
+    public T deleteFirst() {
+        if (null == head) {
+            throw new NoSuchElementException();
+        }
+        Node<T> trash = head;
+        head = head.next;
+        trash.next = null;
+        modCount++;
+        return trash.item;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        Iterator<T> iterator = new Iterator<T>() {
+            private ForwardLinked.Node<T> current = head;
+            private int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return null != current;
+            }
+
+            @Override
+            public T next() {
+                T data = current.item;
+                current = current.next;
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return data;
+            }
+        };
+        return iterator;
+    }
+
+    private static class Node<T> {
+        private T item;
+        private ForwardLinked.Node<T> next;
+
+        Node(T element, ForwardLinked.Node<T> next) {
+            this.item = element;
+            this.next = next;
+        }
+    }
+}
