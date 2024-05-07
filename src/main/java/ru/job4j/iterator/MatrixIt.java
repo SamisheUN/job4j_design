ee67460697ddc62eccb2077f7a6e7a97040292ae
Sammy <SerSam-2007@yandex.ru>
2024-05-07 20:21:10 +0400

    2. Итератор для двухмерного массива int[][]   [#9539 #374183]
    Убран флаг и

--- src/main/java/ru/job4j/iterator/MatrixIt.java
+++ src/main/java/ru/job4j/iterator/MatrixIt.java
@@ -14,17 +14,11 @@ public class MatrixIt implements Iterator<Integer> {
 
     @Override
     public boolean hasNext() {
-        boolean rsl = false;
-        while (row < data.length) {
-            if (column < data[row].length && data[row][column] != 0) {
-                rsl = true;
-                break;
-            } else {
-                column = 0;
-                row++;
-            }
+        while (row < data.length && column == data[row].length) {
+            row++;
+            column = 0;
         }
-        return rsl;
+        return row < data.length;
     }
 
     @Override
