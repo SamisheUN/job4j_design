7260ddea8734cce4ab092ea93681a78fae5efc4b
Sammy <SerSam-2007@yandex.ru>
2024-06-28 03:14:20 +0400

    6. Перевернуть связанный список [#161] первая отправка

--- src/main/java/ru/job4j/collection/RevertLinked.java
+++ src/main/java/ru/job4j/collection/RevertLinked.java
@@ -7,7 +7,7 @@ public class RevertLinked<T> implements Iterable<T> {
     private Node<T> head;
 
     public void add(T value) {
-        Node<T> node = new Node<T>(value, null);
+        Node<T> node = new Node<>(value, null);
         if (head == null) {
             head = node;
             return;
@@ -26,7 +26,7 @@ public class RevertLinked<T> implements Iterable<T> {
         } else {
             Node<T> previous = null;
             Node<T> current = head;
-            Node<T> next = null;
+            Node<T> next;
             while (current != null) {
                 next = current.next;
                 current.next = previous;
@@ -40,7 +40,7 @@ public class RevertLinked<T> implements Iterable<T> {
 
     @Override
     public Iterator<T> iterator() {
-        return new Iterator<T>() {
+        return new Iterator<>() {
             private Node<T> node = head;
 
             @Override
